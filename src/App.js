273695f7a8959e2c2531bd769d341658cdb50823
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-14 20:23:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Products,
+  SingleProduct,
+} from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
